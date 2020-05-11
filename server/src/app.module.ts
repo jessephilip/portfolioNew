be0976a19174e253493d24b119bd38cc8ed4c76e
Jesse <jessephilip@gmail.com>
2020-05-11 07:15:07 -0500

    build and serve client

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,8 +10,13 @@ const user = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE_NAME;
 
+const rootPath = join(__dirname, '..', 'client');
+
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: rootPath,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgres',
