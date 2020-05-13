fba5f0b42d9fc543bf2768903a872a830d3d35f1
Jesse <jessephilip@gmail.com>
2020-05-13 03:36:38 -0500

    update api to return undefined string if no DATABASE_URL present

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const databaseUrl = process.env.DATABASE_URl;
+    const databaseUrl = process.env.DATABASE_URl || 'undefined';
     return databaseUrl;
   }
 }
