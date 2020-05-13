ef05575a6030e181ef1571fe088942d551ed2b91
Jesse <jessephilip@gmail.com>
2020-05-13 05:21:24 -0500

    Set up environments (#3)
    Set up local development environment and Heroku production environment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -6,26 +7,20 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-const user = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_DATABASE_NAME;
-
 const rootPath = join(__dirname, '..', 'client');
+const databaseUrl = process.env.DATABASE_URL;
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: rootPath,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: user,
-      password: password,
-      database: database,
-      entities: [],
-      synchronize: true,
+      url: databaseUrl,
     }),
   ],
   controllers: [AppController],
