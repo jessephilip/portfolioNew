2e8ff7045ab698ceff829e480760f7008911e6dc
Jesse <jessephilip@gmail.com>
2020-05-13 04:52:09 -0500

    remove api from get hello service

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,21 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
   getHello(): string {
-    const withoutProcessEnv = this.configService.get('DATABASE_URL');
-    const databaseUrl = this.configService.get('process.env.DATABASE_URL');
-
-    if (withoutProcessEnv) {
-      return `without ${withoutProcessEnv}`;
-    }
-
-    if (databaseUrl) {
-      return `with ${databaseUrl}`;
-    }
-
-    return 'undefined';
+    return 'Hello World!';
   }
 }
