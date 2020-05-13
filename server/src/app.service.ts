d584ba1d99aa93ab311db535e6a9a7010b622511
Jesse <jessephilip@gmail.com>
2020-05-13 03:26:42 -0500

    update api to return database url

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const databaseUrl = process.env.DATABASE_URl;
+    return databaseUrl;
   }
 }
