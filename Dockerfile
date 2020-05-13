17aa2ffdc3a7a31246f3cb920b1256380b6ffa83
Jesse <jessephilip@gmail.com>
2020-05-13 05:29:01 -0500

    Nuke old profile app and replace with new "something else" app (#4)

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:lts-alpine
+
+EXPOSE 3000
+
+COPY ./server/ ./server/
+
+WORKDIR /server
+
+RUN npm install && npm run build
+
+WORKDIR /
+
+COPY ./client/ ./client/
+
+WORKDIR /client
+
+RUN npm install && npm run build
+
+WORKDIR /
+
+CMD [ "node", "server/dist/server/main.js" ]
---
+++ server/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+const rootPath = join(__dirname, '..', 'client');
+const databaseUrl = process.env.DATABASE_URL;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: rootPath,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: databaseUrl,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
